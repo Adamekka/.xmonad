d35f9ff4ad1400c55c28737a7a30e82cf8d76716
Adam Cvikl <adam.wasd4@gmail.com>
2022-12-02 19:49:26 +0100

    add layouts

--- xmonad.hs
+++ xmonad.hs
@@ -133,3 +133,18 @@ myMouseBindings XConfig {XMonad.modMask = modm} =
       )
       -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
+
+-- Layouts
+myLayout = tiled ||| Mirror tiled ||| Full
+  where
+    -- default tiling algorithm partitions the screen into two panes
+    tiled = Tall nmaster delta ratio
+
+    -- The default number of windows in the master pane
+    nmaster = 1
+
+    -- Default proportion of screen occupied by master pane
+    ratio = 1 / 2
+
+    -- Percent of screen to increment by when resizing panes
+    delta = 3 / 100
