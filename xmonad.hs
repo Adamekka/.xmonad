ac69121f5dcd12861748d07478c6ae68c1fd2326
Adam Cvikl <adam.wasd4@gmail.com>
2022-12-02 16:38:18 +0100

    add comments

--- xmonad.hs
+++ xmonad.hs
@@ -24,8 +24,10 @@ desktop _ = desktopConfig
 
 myTerminal = "alacritty"
 
+-- Whether focus follows the mouse pointer
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
 
+-- Whether clicking on a window to focus also passes the click to the window
 myClickJustFocuses :: Bool
 myClickJustFocuses = False
